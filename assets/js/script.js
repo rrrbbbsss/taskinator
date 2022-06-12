b40cd57cc1a22058ac5a4337c3a11ea05faeefe4
Royce Strange <roycest@swbell.net>
2022-06-11 21:20:36 -0500

    basic input validation and resetting form

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,13 @@ var taskFormHandler = function (event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check input values
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
     // package up data 
     var taskDataObj = {
         name: taskNameInput,
